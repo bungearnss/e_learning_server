94ede7e0c48cf42f24838e3ad48bcffb11736d21
bungearnss <love2554f@hotmail.com>
2023-01-16 21:50:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
 /* require routes */
 var About = require('./routers/About');
 var User = require('./routers/User');
+var Login = require('./routers/Login')
+
+//open port 13000
+var server = app.listen(13000, function () {
+    console.log('Ready on port %d', server.address().port);
+});
 
 app.use(cors());
 app.use(logger('dev'));
@@ -28,5 +27,13 @@ app.use(cookieParser());
 /* use routes */
 app.use('/About', About);
 app.use('/User', User);
+app.use('/Login',Login);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 module.exports = app;
