62656ebc720198cb97bc12edb4a75a4d53f0bcf3
bungearnss <love2554f@hotmail.com>
2023-01-16 21:45:25 +0700

    Update routes

--- app.js
+++ app.js
@@ -13,4 +13,20 @@ app.use(function (req, res, next) {
     next(err);
 });
 
+/* require routes */
+var About = require('./routers/About');
+var User = require('./routers/User');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+
+/* use routes */
+app.use('/About', About);
+app.use('/User', User);
+
 module.exports = app;
